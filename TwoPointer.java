1bfe9153d6cdbb4fd08e5222033c057a8faae155
Kumar Rishabh <kr.rishabh72@gmail.com>
2025-08-28 11:54:31 +0530

    new files

---
+++ TwoPointer.java
@@ -0,0 +1,19 @@
+class TwoPointer{
+
+    public void variableSlidingWindowTemplate(int num[]){
+        int left = 0;
+        for(int right=0; right<num.length; right++){
+
+            //expand the window by expanding the right
+
+            if( 
+                // window is invalid, condition is not met
+                ){
+                //shrink window from left
+                left++;
+            }
+
+            // update answer if valid
+        }
+    }
+}
